4485c3eeb9e1e75c64908cb13ca8e08edeff1f59
Ewin Sutriandi <ewin.sutriandi@gmail.com>
2020-12-13 11:14:42 +0800

    fix title

--- app/src/main/java/simple/example/kursrupiah/MainActivity.java
+++ app/src/main/java/simple/example/kursrupiah/MainActivity.java
@@ -38,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
     JSONObject kursTerbaru;
     DecimalFormat formatUang;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
